82f772c2ea1b45ef4eb11ca568d57eca2c61e112
yuanwei <yuan.akwei@gmail.com>
2018-09-15 21:29:47 +0800

    no message

--- src/com/huofu/plugins/genmloginfo/action/GenMockAction.java
+++ src/com/huofu/plugins/genmloginfo/action/GenMockAction.java
@@ -10,19 +10,26 @@ import com.huofu.plugins.genmloginfo.base.BaseCopyAction;
 public class GenMockAction extends BaseCopyAction {
 
     public String createMethodLogInfo(String methodInfo) {
-        return "cmd=mock\n" +
-                "module=\n" +
-                "env=\n" +
-                "act=switch\n" +
-                "act=mock\n" +
-                "act=clear\n" +
-                "n=" + methodInfo + "\n" +
-                "ex_name=org.apache.thrift.TException\n" +
-                "ex_sleep=10\n" +
-                "ex_redo=true\n" +
-                "ex_after=true\n" +
-                "errorCode=110\n" +
-                "message=测试异常\n" +
-                "";
+        return "method=\n" +
+                "plan=\n" +
+                "exName=\n" +
+                "before=\n" +
+                "time=\n" +
+                "dataJson=\n"
+                ;
+//        return "cmd=mock\n" +
+//                "module=\n" +
+//                "env=\n" +
+//                "act=switch\n" +
+//                "act=mock\n" +
+//                "act=clear\n" +
+//                "n=" + methodInfo + "\n" +
+//                "ex_name=org.apache.thrift.TException\n" +
+//                "ex_sleep=10\n" +
+//                "ex_redo=true\n" +
+//                "ex_after=true\n" +
+//                "errorCode=110\n" +
+//                "message=测试异常\n" +
+//                "";
     }
 }
